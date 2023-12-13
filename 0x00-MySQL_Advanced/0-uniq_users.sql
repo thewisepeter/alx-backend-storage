963f9d48a1abf02e7b4aaccc8612e1d140642075
thewisepeter <rempeterwisdom@gmail.com>
2023-12-13 08:24:42 +0000

    solution 0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
