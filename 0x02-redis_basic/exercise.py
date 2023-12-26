42c2620ad41d00a4c0cdacdc564e8791cf53717d
thewisepeter <rempeterwisdom@gmail.com>
2023-12-26 11:05:28 +0000

    solution exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -34,6 +34,24 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    '''
+        store the history of inputs and outputs
+    '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''
+            wrapper for the decorated function
+        '''
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         # Initialize Redis client and flush the database
@@ -41,6 +59,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         # Generate a random key using uuid
         key = str(uuid.uuid4())
